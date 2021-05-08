17f2a90f026bcdb55f3df572bc7f4b63185f25bb
alexander <zeimedee@gmail.com>
2021-05-08 06:50:26 +0000

    :rocket:portfolio

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import data from "../yourdata"
+import me from '../images/me.jpg'
 
 const About = () => {
   return (
--- src/yourdata.js
+++ src/yourdata.js
@@ -119,7 +119,7 @@ export default {
   aboutParaThree:
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
   aboutImage:
-    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "./images/me.jpg",
 
   //   End About Section ---------------------
 
@@ -185,7 +185,7 @@ export default {
     // },
     {
       img: instagramIcon,
-      url: "https://www.instagram.com/",
+      url: "https://www.instagram.com/zeimedee/",
     },
   ],
 
